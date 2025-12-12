8f12012dcc52ac1e869f6a8b626b59104538ad7a
mayank1828 <144683194+mayank1828@users.noreply.github.com>
2025-12-12 20:46:31 +0530

    Update vite.config.js

--- CampaignApp/vite.config.js
+++ CampaignApp/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Explicitly set root to current directory
+  build: {
+    outDir: 'dist',
+  }
 })
